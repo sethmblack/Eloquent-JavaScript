2d5c3ff1b172d3a834f226f9e0b2cf5e9177bb8b
Seth Black <40257313+sethmblack@users.noreply.github.com>
2018-06-14 10:11:59 -0500

    Exercise completed

---
+++ ch3-Minimum.js
@@ -0,0 +1,7 @@
+// The previous chapter introduced the standard function Math.min that returns its smallest argument. We can do that ourselves now. Write a function min that takes two arguments and returns their minimum.
+
+function min (num1, num2) {
+    return(Math.min(num1, num2));
+}
+
+console.log(min(17, 12));
